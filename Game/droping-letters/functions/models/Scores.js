cedba0187e5d437012e3e91ee64ae2f5651642da
Kazimieras01 <k.simkevicius01@gmail.com>
2024-03-19 15:45:57 +0200

    bbbbbbbbbbbbb

--- Game/droping-letters/functions/models/Scores.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const scoreSchema = new mongoose.Schema({
-  name: String,
-  score: Number });
-
-const ScoreModel = mongoose.model("Score", scoreSchema);
-
-module.exports = ScoreModel;
